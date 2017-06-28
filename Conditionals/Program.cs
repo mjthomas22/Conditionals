fc32f90d53958e476e1910b5d40c3fbf8d10a6d3
mjthomas22 <mthomas22@gmail.com>
2017-06-28 12:44:29 -0500

    commit number two

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,6 +10,34 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is the name on your reservation");
+            string reserveName = Console.ReadLine();
+
+            Console.WriteLine("How many people are in your party");
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What was the cost per meal?");
+            double amountEach = double.Parse(Console.ReadLine()); 
+            double sub = numberOfPeople * amountEach;
+
+           
+
+            double discount;
+
+            if (sub >= 50)
+            {
+                discount = .10;
+                Console.WriteLine("You recieved a 10% discount");
+            }
+            else
+            {
+                discount = .05;
+                Console.WriteLine("You recieved a 5% discount");
+            }
+            double total = sub-(sub * discount);
+            Console.WriteLine("Reservation Name : {0}", reserveName);
+            Console.WriteLine("Subtotal: ${0}", sub);
+            Console.WriteLine("Total after discount ${0}", total);
         }
     }
 }
